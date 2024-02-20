df4ccaa209444b8673eb4abe596435cd5351aaf6
lijie <lijie@hzanchu.com>
2024-02-20 18:10:29 +0800

    nodejs

---
+++ global/console.js
@@ -0,0 +1,7 @@
+console.log(123,222);
+console.info(123)
+console.error(123)
+console.warn(123)
+console.time(123)
+console.timeEnd(123)
+console.log('111111%d11111', 222222222)
---
+++ global/filename.js
@@ -0,0 +1,8 @@
+console.log(__filename)
+console.log(__dirname)
+
+function prinHello() {
+	console.log('你好 世界')
+}
+
+const t = setInterval(prinHello, 1000);
