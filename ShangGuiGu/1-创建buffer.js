a50fdea719ae5fcd9fc4373a6f5fc77c42675bca
lijie <839915978@qq.com>
2024-03-22 23:45:53 +0800

    add

---
+++ ShangGuiGu/1-创建buffer.js
@@ -0,0 +1,8 @@
+// const buf = Buffer.alloc(10);
+// console.log(buf)
+
+// const buf = Buffer.allocUnsafe(10000);
+// console.log(buf)
+
+const buf = Buffer.from([123,123,123,345,345,345,62,2346,1234]);
+console.log(buf)
